6ac40a8db5c31bbb6f773e79038a12807e856f56
zhangkai <wodexintiao@gmail.com>
2017-12-26 10:17:24 +0800

    add testpoint

--- hello_world/hello.cc
+++ hello_world/hello.cc
@@ -16,6 +16,8 @@ const char* ToStr(Local<Value> valStr) {
   // return *value ? *value : "<string conversion failed>";
 }
 void TestString() {
+  // window 下没有问题，但在linux下会报贪婪指针错误
+  // *** Error in `node': malloc(): memory corruption: 0x000000000403a800 ***
   char* str = (char*)malloc(1);
   int len = 1024*20;
   for (int i = 0; i<len; i++) {
@@ -27,13 +29,24 @@ void TestString() {
   strncpy(str_new, str, 11);
   printf("str_new = %s\n", str_new);
 }
+// 用指针的指针做参数可以用于改变指针所指定的值（可用于数据长度扩容）
+void TestPoint(char** p) {
+  char* str = "hello";
+  *p = str;
+}
 void Method(const Nan::FunctionCallbackInfo<v8::Value>& info) {
   if (info[0]->IsString()) {
     const char* str = ToStr(info[0]);
     printf("str = %s, len = %d\n", str, strlen(str));
   }
+  // TestString();
+
+  char* str = "abc";
+  printf("str = %s\n", str);
+  TestPoint(&str);
+  printf("str_new = %s\n", str);
+
   info.GetReturnValue().Set(Nan::New("world").ToLocalChecked());
-  TestString();
 }
 
 void Init(v8::Local<v8::Object> exports) {
