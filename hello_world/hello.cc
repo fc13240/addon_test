e2744ef1c1f0adbf9ebc1a2e4548b0bcb575b1d1
zhangkai <wodexintiao@gmail.com>
2017-12-26 09:49:05 +0800

    a

--- hello_world/hello.cc
+++ hello_world/hello.cc
@@ -15,12 +15,25 @@ const char* ToStr(Local<Value> valStr) {
   // String::Utf8Value value(valStr);
   // return *value ? *value : "<string conversion failed>";
 }
+void TestString() {
+  char* str = (char*)malloc(1);
+  int len = 1024*20;
+  for (int i = 0; i<len; i++) {
+    str[i] = (char)(97);
+  }
+  str[len] = '\0';
+  printf("str = %s, %d\n", str, strlen(str));
+  char* str_new = (char*)malloc(11);
+  strncpy(str_new, str, 11);
+  printf("str_new = %s\n", str_new);
+}
 void Method(const Nan::FunctionCallbackInfo<v8::Value>& info) {
   if (info[0]->IsString()) {
     const char* str = ToStr(info[0]);
     printf("str = %s, len = %d\n", str, strlen(str));
   }
   info.GetReturnValue().Set(Nan::New("world").ToLocalChecked());
+  TestString();
 }
 
 void Init(v8::Local<v8::Object> exports) {
